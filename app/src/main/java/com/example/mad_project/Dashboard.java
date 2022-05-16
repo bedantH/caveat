2c5df27ceb5660a088c3794041f630681c2742c5
Pratham Yadav <72993471+ypratham@users.noreply.github.com>
2022-05-16 15:44:31 +0530

    Changed fragment navigation

--- app/src/main/java/com/example/mad_project/Dashboard.java
+++ app/src/main/java/com/example/mad_project/Dashboard.java
@@ -2,13 +2,20 @@ package com.example.mad_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.mad_project.ui.gallery.GalleryFragment;
+import com.example.mad_project.ui.home.HomeFragment;
+import com.example.mad_project.ui.slideshow.SlideshowFragment;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,40 +26,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.mad_project.databinding.ActivityDashboardBinding;
 
 public class Dashboard extends AppCompatActivity {
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityDashboardBinding binding;
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_dashboard);
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_dashboard, new HomeFragment()).commit();
 
-        setSupportActionBar(binding.appBarDashboard.toolbar);
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_dashboard);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open, R.string.navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.dashboard, menu);
-        return true;
-    }
+    public boolean onNavigationItemSelected(MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_gallery:
+                fragment = new GalleryFragment();
+                break;
+            case R.id.nav_slideshow:
+                fragment = new SlideshowFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_dashboard, fragment).commitNow();
+        return true ;
+    };
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_dashboard);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
